3ddeabbdf0bef269a543d2c23ca0df44252240e1
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-11-13 22:01:42 -0500

    stop point after update HTML to Blogar dispaly

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=BlogPosts}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
